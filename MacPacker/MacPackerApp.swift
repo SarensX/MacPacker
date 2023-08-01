d506974936af09003089a1aaf7a9ec0538a137c6
Arenswald Stephan <stephan.arenswald@mercedes-benz.com>
2023-08-01 08:32:06 +0200

    Initial Commit

---
+++ MacPacker/MacPackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPackerApp.swift
+//  MacPacker
+//
+//  Created by Arenswald, Stephan (059) on 01.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MacPackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacPackerUITests/MacPackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MacPackerUITestsLaunchTests.swift
+//  MacPackerUITests
+//
+//  Created by Arenswald, Stephan (059) on 01.08.23.
+//
+
+import XCTest
+
+final class MacPackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
