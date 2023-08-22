1679bbfc3518ce11403ac63b152113a5cbabb0f4
Arenswald Stephan <stephan.arenswald@mercedes-benz.com>
2023-08-22 03:40:52 +0200

    feat: clear cache

--- MacPacker/View/ContentView.swift
+++ MacPacker/View/ContentView.swift
@@ -8,18 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let applicationSupportDirectory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first
+
     var body: some View {
         VStack {
             ArchiveBrowserView()
         }
         .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Button {
-                    NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: "/Users/sarensw/Library/Containers/com.sarensx.MacPacker/Data/Library/Application Support/")
+            ToolbarItemGroup(placement: .primaryAction) {
+                Menu {
+                    Button {
+                        if let url = applicationSupportDirectory {
+                            NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: url.path)
+                        }
+                    } label: {
+                        Text("Open cache directory")
+                    }
+                    .help("Show application support folder")
+                    .disabled(applicationSupportDirectory == nil)
+                    Button {
+                        clearCache()
+                    } label: {
+                        Text("Clear cache")
+                    }
+                    .help("Clears all content from the cache")
+                    .disabled(applicationSupportDirectory == nil)
                 } label: {
-                    Image(systemName: "folder.badge.gearshape")
+                    Image(systemName: "gear")
                 }
-                .help("Show application support folder")
+            }
+        }
+    }
+    
+    /// Clears the cache be deleting the ta directory in the applciation support directory
+    func clearCache() {
+        if let url = applicationSupportDirectory {
+            do {
+                try FileManager.default.removeItem(at: url.appendingPathComponent("ta", conformingTo: .directory))
+            } catch {
+                print("Could not clear cache because...")
+                print(error)
             }
         }
     }
